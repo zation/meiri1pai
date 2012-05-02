2422cc6a2be92d3ada153dcb3fde3fbc83a7059c
Zation <zation1@gmail.com>
2012-05-02 21:19:23 +0800

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meiri1pai::Application.config.secret_token = '18be4498480fb0b4a5a6939e15dfdec49df07a17ac92f0f35ad2d30ecc122572e071429daacb56d596c833df617e800c234fb18d1c4e776c427885eb62d8d9e1'
