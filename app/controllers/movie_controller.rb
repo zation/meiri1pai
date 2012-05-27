1939053023630f621459763fc0c7d52109a2a0d6
Zation <zation1@gmail.com>
2012-05-27 18:03:49 +0800

    order by weibo_id DESC

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -2,7 +2,7 @@ require 'weibo_service'
 
 class MovieController < ApplicationController
 	def show
-		@images = Image.where(:user_id => params['user_id'])
+		@images = Image.where(:user_id => params['user_id']).order('weibo_id DESC')
 	end
 end
 
